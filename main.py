f15f258d21e2b39010dfbe1811e7cb19904c58c4
raceee <race.peterson@gmail.com>
2021-10-10 14:25:05 -0600

    orbit made

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ if the orbit of Qc2 is attracted to a 2 cycle then we plot c2, q1) and (c2, q2).
 """
 
 
+from typing import Collection
 import numpy
 import matplotlib
 
@@ -34,17 +35,39 @@ def two_cycle_check(orbital):
     try:
         if orbital[len(orbital) - 1] == orbital[len(orbital) - 3]:
             print("fp found")
-            return orbital[len(orbital) - 1] , orbital[len(orbital) - 3]
+            return orbital[len(orbital) - 1], orbital[len(orbital) - 3]
+        else: 
+            pass
     except:
         pass
+
+def make_orbit(func, lambda_int, interval):
+    for l in lambda_int:
+        for x in interval:
+            orbital_of_x = [x]
+            a = func(x,l)
+            orbital_of_x.append(a)
+            for _ in range(500):
+                a = func(a, l)
+                orbital_of_x.append(a) # TODO: Where do I store plots to be plotted?
+                
 
 
 
 def func_one(lambda_int, interval):
     for l in lambda_int:
-
-
+        continue
     pass
+
+
+
+
+
+
+
+
+
+
 
 def func_two():
     pass
