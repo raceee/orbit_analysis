b8dfff4d84d2d31586a2bf588a9ccdaa7940a583
raceee <race.peterson@gmail.com>
2021-10-10 19:30:14 -0600

    bug fixes

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def fp_check(orbital):
     "returns single val"
     try:
         if orbital[len(orbital) - 1] == orbital[len(orbital) - 2]:
-            print("fp found")
+            # print("fp found")
             return orbital[len(orbital) - 1]
         else:
             return False
@@ -36,7 +36,7 @@ def two_cycle_check(orbital):
     "returns tuple"
     try:
         if orbital[len(orbital) - 1] == orbital[len(orbital) - 3] and orbital[len(orbital) - 2] == orbital[len(orbital) - 4]: 
-            print("two cycle found")
+            # print("two cycle found")
             return orbital[len(orbital) - 1], orbital[len(orbital) - 2]
         else: 
             return False
@@ -46,8 +46,9 @@ def two_cycle_check(orbital):
 
 def vis(graph):
     fig = plt.figure()
-    ax = fig.add_subplot(projection="2d")
+    ax = fig.add_subplot()
     for pair in graph:
+        print(pair)
         ax.scatter(pair[0], pair[1])
     plt.show()
     
@@ -57,28 +58,51 @@ def make_orbit(func, lambda_int, interval):
     for l in lambda_int:
         for x in interval:
             orbital_of_x = [x]
-            a = func(x,l)
+            a = func(l,x)
             orbital_of_x.append(a)
-            for _ in range(500):
-                a = func(a, l)
+            for _ in range(50):
+                print("lambda: {} x: {} iter: {}".format(l,x,_))
+                a = func(l, a)
                 orbital_of_x.append(a)
                 # check orbit
+
                 fp = fp_check(orbital_of_x)
-                ppone, pptwo = two_cycle_check(orbital_of_x)
-                if fp:
-                    # points to plot (c, x)
+                print("fp: ", fp)
+                if fp != False:
                     all_points.append([l,fp])
-                if ppone:
-                    all_points.append([l,ppone])
-                    all_points([l,pptwo])
+                else:
+                    period = two_cycle_check(orbital_of_x)
+                    print("period: ", period)
+                    if period != False:
+                        all_points.append([l,period[0]])
+                        all_points.append([l,period[1]])
+                    else:
+                        pass
+        if l == 1.2:
+            break
+    print("len of all points: ", len(all_points))
     vis(all_points)
 
 
 def func_one(l, x):
-    return l * np.sin(x)
+    return l * np.sin(x) # maybe this 
 
+
+def func_two(l,x):
+    return l * np.cos(x)
+
+
+def func_three(l, x):
+    d = (x - x ** 3)/3
+    f = l * d
+    return f
 
 if __name__ == "__main__":
-    interval_one = list(range(interval_one[0], interval_one[1], 0.001))
-    lambda_interval_one = list(range(lambda_interval_one[0], lambda_interval_one[1], 0.001))
-    make_orbit(func_one, lambda_int=lambda_interval_one, interval=interval_one)
+    interval_one = [num/10 for num in range(31)] + [3.14]
+    print(interval_one)
+    lambda_one = [1]+[(num/10)+1 for num in range(1,215)]
+    lambda_one = lambda_one[:22]+[3.14]
+    print("---------------------------------------------------------")
+    print(lambda_one)
+    make_orbit(func_one, lambda_int=lambda_one, interval=interval_one)
+    # make_orbit(func_one, lambda_int=lambda_interval_one, interval=interval_one)
